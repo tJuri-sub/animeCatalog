62a829c03f6737decf228da640ab7203798939e3
tJuri-sub <juricuber@gmail.com>
2026-01-31 00:24:45 +0800

    user model

---
+++ server/models/admin.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const User = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin"],
+    default: "admin",
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const UserData = mongoose.model("User", User);
+
+export default UserData;
