974d8ccf72f0f1b8c087f018310472dd58780942
tJuri-sub <juricuber@gmail.com>
2026-02-06 20:21:01 +0800

    configure routes and cors

--- server/index.js
+++ server/index.js
@@ -2,17 +2,28 @@ import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import userRouter from "./routers/userRouter.js";
+import animeRouter from "./routers/animeRouter.js";
+import contactRouter from "./routers/contactRouter.js";
 
 dotenv.config();
 const app = express();
 
+const corsOption = {
+  origin: "http://localhost:5173",
+  credentials: true,
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(corsOption));
+app.use(cookieParser());
 
-app.use("/api", userRouter);
+app.use("/user", userRouter);
+app.use("/anime", animeRouter);
+app.use("/contact", contactRouter);
 
 mongoose
   .connect(process.env.MONGO_URI)
